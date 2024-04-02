17389b5d06f730d92d9692e906bf34676d6c605e
Ashwitha Kassetty <47321440+avk1943@users.noreply.github.com>
2024-04-01 21:55:19 -0700

    Stage 01 - Data Ingestion notebook added

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from textSummarizer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
-from textSummarizer.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
-from textSummarizer.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
-from textSummarizer.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
-from textSummarizer.pipeline.stage_05_model_evaluation import ModelEvaluationTrainingPipeline
+#from textSummarizer.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
+#from textSummarizer.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
+#from textSummarizer.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
+#from textSummarizer.pipeline.stage_05_model_evaluation import ModelEvaluationTrainingPipeline
 from textSummarizer.logging import logger
 
 
-STAGE_NAME = "Data Ingestion stage"
+STAGE_NAME = "Data Ingestion Stage"
 try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
    data_ingestion = DataIngestionTrainingPipeline()
---
+++ src/textSummarizer/pipeline/stage_01_data_ingestion.py
@@ -0,0 +1,21 @@
+from textSummarizer.config.configuration import ConfigurationManager
+from textSummarizer.components.data_ingestion import DataIngestion
+from textSummarizer.logging import logger
+
+class DataIngestionTrainingPipeline:
+    def __init__(self):
+        pass
+
+    def main(self):
+        # initialize the configuration manager class
+        config = ConfigurationManager() 
+
+        # get the data ingestion configuration from the class
+        data_ingestion_config = config.get_data_ingestion_config()
+    
+        # call the data ingestion components
+        data_ingestion = DataIngestion(config=data_ingestion_config)
+
+        # call the two methods: download_file() and extract_zip_file() in DataIngestion class
+        data_ingestion.download_file()
+        data_ingestion.extract_zip_file()
